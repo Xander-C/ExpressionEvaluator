c9721632ae6bab8c05d9d923a49ee95dac85c802
Xander-C <1019104544@qq.com>
2021-11-09 13:51:14 +0800

    refactor:  改进运算符比较函数

--- src/js/modules/cmpOperator.js
+++ src/js/modules/cmpOperator.js
@@ -2,44 +2,38 @@ import {
     Result
 } from "./cmpOpResult.js"
 
-const smallerThanPlus = "#([{";
-const biggerThanTimes = "^logsqrtabs)]}";
-const biggerThanPower = "logsqrtabs)]}";
-const biggerThanUnary = ")]}";
+const priorityMap = {
+    "+": 1,
+    "-": 1,
+    "*": 2,
+    "/": 2,
+    "^": 3,
+    "^": 3,
+    "log": 4,
+    "sqrt": 4,
+    "abs": 4,
+};
 
 const seq = "#{[(+-*/^)]}"
 
 export const cmpOperator = (inStack, char) => {
     console.log("comparing: " + inStack + " and " + char);
     switch (char) {
-        case '+':
-        case '-':
-            return smallerThanPlus.indexOf(inStack) != -1 ? Result.CHAR : Result.STACK;
-        case '*':
-        case '/':
-            return biggerThanTimes.indexOf(inStack) != -1 ? Result.STACK : Result.CHAR;
-        case '^':
-            return biggerThanPower.indexOf(inStack) != -1 ? Result.STACK : Result.CHAR;
-        case 'log':
-        case 'sqrt':
-        case 'abs':
-            return biggerThanUnary.indexOf(inStack) != -1 ? Result.STACK : Result.CHAR;
         case '(':
             return Result.CHAR;
         case ')':
             return inStack == '(' ? Result.CLOSE_BRACKET : Result.STACK;
-        case '#':
-            return inStack == '#' ? Result.CLOSE_BRACKET : Result.STACK;
         case '[':
-            return inStack == ')' ? Result.STACK : Result.CHAR;
+            return Result.CHAR;
         case ']':
             return inStack == '[' ? Result.CLOSE_BRACKET : Result.STACK;
         case '{':
-            return (inStack == ')' || inStack == ']') ? Result.STACK : Result.CHAR;
+            return Result.CHAR;
         case '}':
             return inStack == '{' ? Result.CLOSE_BRACKET : Result.STACK;
+        case '#':
+            return inStack == '#' ? Result.CLOSE_BRACKET : Result.STACK;
         default:
-            alert("character error");
-            break;
+            return priorityMap[inStack] > priorityMap[char] ? Result.STACK : Result.CHAR;
     }
 }
